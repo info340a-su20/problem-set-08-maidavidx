29b3506aa58ed621b290d943f2ea99dfbce62e10
maidavidx <maid2@uw.edu>
2020-12-29 20:32:43 -0800

    finished problem-a

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -6,3 +6,55 @@ const EXAMPLE_SENATORS = [
 ];
 
 /* Your code goes here */
+export class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>US Senators 2019</h1>
+        <SenatorTable senators={this.props.senators} />
+      </div>
+    );
+  }
+}
+
+export class SenatorTable extends Component {
+  render() {
+    let headerNames = ['Name', 'State', 'Phone', 'Twitter'];
+    let senatorRows = this.props.senators.map((senator) => 
+      <SenatorRow key={senator.id} senator={senator}/>
+    );
+    return (
+      <table className="table table-bordered">
+        <TableHeader cols={headerNames}/>
+        <tbody>{senatorRows}</tbody>
+      </table>
+    );
+  }
+}
+
+export class TableHeader extends Component {
+  render () {
+    let columnNames = this.props.cols.map((columnName) => 
+    <th key={columnName}>{columnName}</th>
+    );
+    return (
+      <thead>
+        <tr>{columnNames}</tr>
+      </thead>
+    );
+  }
+}
+
+export class SenatorRow extends Component {
+  render() {
+    let senatorInfo = this.props.senator;
+    return (
+      <tr>
+        <td key={senatorInfo.name}>{senatorInfo.name}</td>
+        <td key={senatorInfo.party.substring(0,1) + " - " + senatorInfo.state}>{senatorInfo.party.substring(0,1)+" - "+senatorInfo.state}</td>
+        <td key={senatorInfo.phone}><a href={"tel:" + senatorInfo.phone}>{senatorInfo.phone}</a></td>
+        <td key={senatorInfo.twitter}><a href={"https://twitter.com/" + senatorInfo.twitter}>{"@" + senatorInfo.twitter}</a></td>
+      </tr>
+    );
+  }
+}
--- problem-a/src/index.js
+++ problem-a/src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './App';
+import Senators from './senators.json';
 
 //render the App component here!
+
+ReactDOM.render(<App senators = { Senators } />, document.getElementById('root'));
